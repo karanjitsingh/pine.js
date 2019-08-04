d526a57601034b622a34be8e906833f4196c49b2
Karan Jit Singh <karan1866@gmail.com>
2019-08-04 23:10:27 +0530

    consolidated

--- data/consolidate.py
+++ data/consolidate.py
@@ -8,14 +8,18 @@ folder =  os.path.join("data", "BINANCE_BTCUSDT") if len(sys.argv) == 1 else sys
 
 dumpPath = os.path.join(os.getcwd(), folder)
 
-dumpJsons = list(filter(lambda file: re.match("^\d{4}-\d{2}-\d{2}\.json$", file), os.listdir(dumpPath)))
+dumpJsons = list(filter(lambda file: re.match("^\d{4}-\d{2}-\d{2}\.[12]\.json$", file), os.listdir(dumpPath)))
+
+print(dumpJsons)
+
+consolidatedData = []
 
 for i in range(len(dumpJsons)):
     with open(path.join(dumpPath, dumpJsons[i]), "r") as file:
         content = file.read()
 
-    print(dumpJsons[i])
-
     data = json.loads(content)
 
-    print(len(data))
+    consolidatedData = consolidatedData + data
+
+print(consolidatedData)
