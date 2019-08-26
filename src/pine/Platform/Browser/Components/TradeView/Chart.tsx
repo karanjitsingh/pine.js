86e7a1c64a37d19eb0b6fa9d1f3b24757f00e6ea
Karan Jit Singh <karan1866@gmail.com>
2019-08-26 08:30:20 +0530

    applied series interface

--- src/pine/Platform/Browser/Components/TradeView/Chart.tsx
+++ src/pine/Platform/Browser/Components/TradeView/Chart.tsx
@@ -1,29 +1,61 @@
 import * as React from 'react';
-import * as LighweightCharts from '../../lib/lightweight-charts';
+import * as LightweightCharts from '../../lib/lightweight-charts';
 import { Plot } from 'Model/Data/Trading';
+import { ChartData } from 'Model/Platform/Contracts';
 
-export class Chart extends React.Component<Plot> {
+export class ChartProps {
+    data: ChartData;
+}
+
+interface Series {
+    mainSeries: LightweightCharts.ISeriesApi<'Candlestick'>;
+    volumeData: LightweightCharts.ISeriesApi<'Histogram'>;
+    indicators: LightweightCharts.ISeriesApi<'Line' | 'Area'>[];
+}
+
+export class Chart extends React.Component<ChartProps> {
 
     private chartContainerRef: React.RefObject<HTMLDivElement>;
+    private chart: LightweightCharts.IChartApi;
 
+    private series: LightweightCharts.ISeriesApi<any>[]
 
-    public constructor(props: Plot) {
+    public constructor(props: ChartProps) {
         super(props);
         this.chartContainerRef = React.createRef<HTMLDivElement>();
     }
 
     public componentDidMount() {
         if(this.chartContainerRef.current) {
-            LighweightCharts.createChart(this.chartContainerRef.current)
+            this.chartContainerRef.current.onresize = this.chartResize.bind(this);
+
+            this.chart = LightweightCharts.createChart(this.chartContainerRef.current)
+            var series = this.chart.addAreaSeries({
+
+            })
+
+            // this.chart.priceScale().
         }
-        // charts.createChart(this.)
     }
 
-    public shouldComponentUpdate() {
-        return false;
+    public shouldComponentUpdate(nextProps: ChartProps) {
+        if(this.chart) {
+            
+            return false;
+        }
+
+        return true;
     }
 
     public render() {
         return <div ref={this.chartContainerRef}></div>;
+    }
+
+    private updateChart(data: ChartData) {
+        // this.chart.
+    }
+
+    private chartResize() {
+
     }
 }
